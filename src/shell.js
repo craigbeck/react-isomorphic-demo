918a0f38091f16b58403b9d29e237581818d0107
Craig Beck <luckybonza@gmail.com>
2016-01-07 10:02:39 -0800

    add build for statically rendered app shell

---
+++ src/shell.js
@@ -0,0 +1,14 @@
+// import React from "react";
+// import ReactDOMServer from "react-dom/server";
+import App from "./components/App";
+
+// if (typeof document !== 'undefined') {
+//   const app = React.createElement(App);
+//   ReactDOMServer.render(app, document);
+// }
+
+/* IMPORTANT!
+ * You must export a component:
+ * using module.exports
+ */
+module.exports = App;
